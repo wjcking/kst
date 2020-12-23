f58467c133852c7c84ce0f183d073e1464c4f886
wjcking <wjcking@hotmail.com>
2020-12-23 23:17:17 +0800

    human

--- animal-requires/controllers/app.js
+++ animal-requires/controllers/app.js
@@ -8,18 +8,23 @@
     shim: {
         'world': {
             deps: ['animalWorld'],
-            // use the global 'Backbone' as the module name.
             exports: 'world'
+        },
+        'human': {
+             //deps: ['human'],
+            exports: 'human'
         }
     }
 });
 //var vue = "outside";    document.writeln(vue);
 //page load when
-require(['cat', 'dog', 'world'], function (cat, dog, world) {
- 
+require(['cat', 'dog', 'world', 'human'], function (cat, dog, world, human) {
+
     cat.say();
     cat.mine();
     dog.say();
     dog.mine("touch-dog missons everydays");
     world.world();
+    human();
+    alert(human);
 });
---
+++ animal-requires/controllers/human.js
@@ -0,0 +1,3 @@
+﻿window.human = function () {
+    document.writeln("人与自然！");
+}
